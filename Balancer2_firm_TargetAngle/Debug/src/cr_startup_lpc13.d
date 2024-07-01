3a6d6a1120cecbc58504c50907e8eff10f581b29
13lackade <13lackade@gmail.com>
2024-07-01 13:51:04 +0900

    simplify

--- Balancer2_firm_TargetAngle/Debug/src/cr_startup_lpc13.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc13.o src/cr_startup_lpc13.d: ../src/cr_startup_lpc13.c \
- Y:/LPCXpresso/Balancer2/Balancer2_firm_TargetAngle/lib/system_LPC13xx.h
-
-Y:/LPCXpresso/Balancer2/Balancer2_firm_TargetAngle/lib/system_LPC13xx.h:
